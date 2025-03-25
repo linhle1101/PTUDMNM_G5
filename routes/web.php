da4083786768e18e35211c3e55622f4ddaaf5277
linhle1101 <mis.linhle@gmail.com>
2025-03-25 16:05:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,32 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/vidu1', function () {
-    return 'Xin chào';
-});
-Route::get('/vidu1','App\Http\Controllers\ViDuController@Linhle');
-
-Route::get('/vidu2','App\Http\Controllers\ViduController@vidu2');
-
-Route::post('/tinhtong','App\Http\Controllers\ViDuController@tinhtong');
-
-Route::get('/baitap1','App\Http\Controllers\BaiTapController@baitap1');
-Route::post('/themtheloai','App\Http\Controllers\BaiTapController@themtheloai');
-
-Route::get('/baitap2','App\Http\Controllers\BaiTapController@baitap2');
-Route::post('/themtheloai2','App\Http\Controllers\BaiTapController@themtheloai2');
-
-Route::get('/layouts/trang1','App\Http\Controllers\ViduLayoutController@trang1');
-
-Route::get('/sach','App\Http\Controllers\ViduLayoutController@sach');
-Route::get('/sach/theloai/{id}','App\Http\Controllers\ViduLayoutController@theloai');
-Route::get('/sach/detail/{id}','App\Http\Controllers\ViduLayoutController@detail');
-
-
-Route::get('/sach/detail/{id}','App\Http\Controllers\ViduLayoutController@detail');
-
-Route::get('/ten','App\Http\Controllers\ViDuController@ten');
-
-
-Route::get('/inten','App\Http\Controllers\ViduController@inten');
