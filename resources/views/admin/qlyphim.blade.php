76a371e5c3a9fac5dc3e48ec397afc1c963ae26e
linhle1101 <mis.linhle@gmail.com>
2025-04-09 18:20:47 +0700

    update đăng xuất

--- resources/views/admin/qlyphim.blade.php
+++ resources/views/admin/qlyphim.blade.php
@@ -1,12 +1,5 @@
 <html>
 <head>
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <link href="https://cdn.datatables.net/1.13.6/css/jquery.dataTables.min.css" rel="stylesheet">
-    <script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
-    <link rel="stylesheet" href="{{asset('css/qly.css')}}">
 </head>
 
 <body>
